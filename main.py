4d4c3e48bcd9fe4915c45499d2ed391e284744eb
Dominic Needham <dominic.needham@bt.com>
2022-08-08 13:37:53 +0100

    Add external temperature to mqtt state

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pico_temperature import PicoTemperature
 from scheduler import Scheduler
 from clock import Clock
-from apps import Apps
+from apps import Apps, App
 from pomodoro import Pomodoro
+from temperature import Temperature
 from time_set import TimeSet
 from wifi import WLAN
 from mqtt import MQTT
@@ -26,6 +27,7 @@ scheduler = Scheduler()
 wlan = WLAN(scheduler)
 mqtt = MQTT(scheduler)
 pico_temperature = PicoTemperature(scheduler, mqtt)
+temperature = Temperature(mqtt)
 apps = Apps(scheduler)
 for App in APP_CLASSES:
     apps.add(App(scheduler))
---
+++ temperature.py
@@ -0,0 +1,24 @@
+from machine import SoftI2C
+from machine import Pin
+from ds3231_port import DS3231
+from util import singleton
+from mqtt import MQTT
+
+
+@singleton
+class Temperature:
+    def __init__(self, mqtt: MQTT):
+        self.mqtt = mqtt
+        rtc_i2c = SoftI2C(scl=Pin(7), sda=Pin(6), freq=100000)
+        self.ds = DS3231(rtc_i2c)
+        mqtt.register_state_callback("temperature", self.get_temperature)
+        pass
+
+    def get_time(self):
+        return self.ds.get_time()
+
+    def save_time(self, t):
+        return self.ds.save_time(t)
+
+    def get_temperature(self):
+        return self.ds.get_temperature()
